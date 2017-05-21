6a4e9204adebc0b4654ae523eb0a9bf6b1ead507
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-21 14:30:08 +0300

    Ex8

---
+++ Ex8/Ex8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 0;
+            BigInteger factorialDividend = 1;
+            BigInteger factorialDivisor = 1;
+
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                for (int i = 2 * n; i > n + 1; i--)
+                {
+                    factorialDividend = factorialDividend * i;
+                }
+                for (int i = n; i > 0; i--)
+                {
+                    factorialDivisor = factorialDivisor * i;
+                }
+
+                result = factorialDividend / factorialDivisor;
+                Console.WriteLine(result);
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
