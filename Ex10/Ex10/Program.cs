b2bc4c690bc214622df730fe0477d4b35e581956
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-21 15:17:00 +0300

    Ex10

---
+++ Ex10/Ex10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of N (N<20)");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int counter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int a = i; counter< n-1; a++)
+                {
+                    Console.Write("{0,3}",a);
+                    counter++;
+                }
+                Console.WriteLine();
+                counter = 0;
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
